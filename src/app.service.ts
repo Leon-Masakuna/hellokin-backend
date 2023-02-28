d14ada30cfea67f87ec8f20fc371b36da20f2fb2
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-02-28 14:21:09 +0100

    feat : server initialization

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! I am Léon and I am using Nestjs for HelloKin backend server';
+  }
+
+  getSomething(): string {
+    return 'Something can be also sent back too';
   }
 }
