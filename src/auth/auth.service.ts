ff044fea1c4137605cd541403d43e198d668e1c6
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-03-02 09:34:38 +0100

    feat : auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async signUp(SignUpDto) {
+    const { phoneOrEmail, fullName, userName, password } = SignUpDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userModel.create({
+      phoneOrEmail,
+      fullName,
+      userName,
+      password: hashedPassword,
+    });
+  }
+}
---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  @Prop()
+  phoneOrEmail: string;
+
+  @Prop()
+  fullName: string;
+
+  @Prop({ unique: [true, 'Duplicate user name entered'] })
+  userName: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
