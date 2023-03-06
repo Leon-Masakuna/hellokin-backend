cf40992d2c6ed31489ccfcff199ea25669bf9dfc
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-03-06 14:27:00 +0100

    feat : create new user and get connected to the application

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtService: JwtService,
   ) {}
 
-  async signUp(SignUpDto) {
-    const { phoneOrEmail, fullName, userName, password } = SignUpDto;
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { phoneOrEmail, fullName, userName, password } = signUpDto;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -22,5 +26,29 @@ export class AuthService {
       userName,
       password: hashedPassword,
     });
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
+
+  async login(loginDto: LoginDto): Promise<{ token: string; user: object }> {
+    const { userName, password } = loginDto;
+
+    const user = await this.userModel.findOne({ userName });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid user name or password');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid user name or password');
+    }
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token, user };
   }
 }
