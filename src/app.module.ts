cfb9b653986cdc3399121af439a52a5423f3a64b
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-04-14 10:42:21 +0200

    feat : generate CRUD for videos posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.DB_CONNEXION),
     AuthModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
